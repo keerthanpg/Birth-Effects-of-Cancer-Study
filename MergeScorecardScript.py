83f60d18503b60b31c27770ad0b56c1390940ac4
keerthanpg <keerthanpg@gmail.com>
2016-05-27 07:54:07 +0530

    Generated Scorecard JSON

--- MergeScorecardScript.py
+++ MergeScorecardScript.py
@@ -56,4 +56,14 @@ print len(ScorecardList)
 print i
 print j
 
+from csv import DictWriter
 
+players = [{'dailyWinners': 3, 'dailyFreePlayed': 2, 'user': 'Player1', 'bank': 0.06},
+{'dailyWinners': 3, 'dailyFreePlayed': 2, 'user': 'Player2', 'bank': 4.0},
+{'dailyWinners': 1, 'dailyFree': 2, 'user': 'Player3', 'bank': 3.1},            
+{'dailyWinners': 3, 'dailyFree': 2, 'user': 'Player4', 'bank': 0.32}]
+
+with open('Scorecard.csv','w') as outfile:
+    writer = DictWriter(outfile, ('Chemical Name', 'CAS No','References', 'Effect'))
+    writer.writeheader()
+    writer.writerows(players)
