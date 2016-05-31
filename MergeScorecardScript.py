487aa231e867f8c6d0b8ae411566e49d2be335a2
keerthanpg <keerthanpg@gmail.com>
2016-05-31 06:38:43 +0530

    Rectified Errors in MergeScoreCard

--- MergeScorecardScript.py
+++ MergeScorecardScript.py
@@ -26,59 +26,89 @@ DT_list = []
 for row_index in xrange(1, sheet_C.nrows):
     d = {keys[col_index]: sheet_C.cell(row_index, col_index).value 
          for col_index in xrange(sheet_C.ncols)}
-    d['Effect']='Cancer'
+    d['Effect']=str.encode('Cancer', 'utf-8')
     ScorecardList[d['CAS No']]=d
-print len(ScorecardList)
+    #print ScorecardList[d['CAS No']]
+#print len(ScorecardList)
 i=0
 for row_index in xrange(1, sheet_RT.nrows):
     d = {keys[col_index]: sheet_RT.cell(row_index, col_index).value 
          for col_index in xrange(sheet_C.ncols)}
     i=i+1
-    print i
+    #print i
     if d['CAS No']in ScorecardList:
-        print ('CAS Matched %s', d['CAS No'] )
-        ScorecardList[d['CAS No']]['Effect']=ScorecardList[d['CAS No']]['Effect'] + ', Reproductive Toxicity'
+        #print ScorecardList[d['CAS No']]
+        #print ('CAS Matched for RT %s', d['CAS No'] )
+        
+        ScorecardList[d['CAS No']]['Effect']=ScorecardList[d['CAS No']]['Effect'] + str.encode(', Reproductive Toxicity', 'utf-8')
+
+        #print ScorecardList[d['CAS No']]
     else:
+        d['Effect']=str.encode('Reproductive Toxicity', 'utf-8')
         ScorecardList[d['CAS No']]=d
-print len(ScorecardList)
+#print len(ScorecardList)
 
 j=0
 for row_index in xrange(1, sheet_DT.nrows):
     d = {keys[col_index]: sheet_DT.cell(row_index, col_index).value 
          for col_index in xrange(sheet_C.ncols)}
     j=j+1
-    print j
+    #print j
     if d['CAS No']in ScorecardList:
-        print ('CAS Matched %s', d['CAS No'] )
-        ScorecardList[d['CAS No']]['Effect']=ScorecardList[d['CAS No']]['Effect'] + ', Developmental Toxicity'
+        #print ('CAS Matched for DT%s', d['CAS No'] )
+        
+        #print ScorecardList[d['CAS No']]
+        ScorecardList[d['CAS No']]['Effect']=ScorecardList[d['CAS No']]['Effect'] + str.encode(', Developmental Toxicity', 'utf-8')
     else:
+        d['Effect']=str.encode('Developmental Toxicity', 'utf-8')
         ScorecardList[d['CAS No']]=d
-print len(ScorecardList)
-print i
-print j
+#print len(ScorecardList)
+#print i
+#print j
 
 from csv import DictWriter
+import unicodedata
 
+
+'''def convert(input):
+    if isinstance(input, dict):
+        return dict((convert(key), convert(value)) for key, value in input.iteritems())
+    elif isinstance(input, list):
+        return [convert(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
+
+Scorecard_String=convert(ScorecardList)'''
 Scorecard_List=[]
 for [k,v] in ScorecardList.iteritems():
-    newdict=ast.literal_eval(v)
-    newdict = ast.literal_eval(newdict)
-    Scorecard_List.append(newdict)
+    Scorecard_List.append(v)
+    
+    
+error=[]    
 
 with open('Scorecard.csv','w') as outfile:
     writer = DictWriter(outfile, ('Chemical Name', 'CAS No','References', 'Effect'))
     writer.writeheader()
-    writer.writerows(Scorecard_List)
-    
+    for i in xrange(len(Scorecard_List)):        
+        try:
+            writer.writerow(Scorecard_List[i])
+            
+        except :            
+            #Scorecard_List[i]['Chemical Name']=Scorecard_List[i]['Chemical Name'].decode(encoding="utf-8", errors="strict")
+            Scorecard_List[i]['Chemical Name']=unicodedata.normalize('NFKD', Scorecard_List[i]['Chemical Name']).encode('ascii','ignore')
+                       
+            try:
+                writer.writerow(Scorecard_List[i])
+            except Exception as ex:
+                template = "An exception of type {0} occured. Arguments:\n{1!r}"
+                message = template.format(type(ex).__name__, ex.args)
+                print Scorecard_List[i] 
+                print message
+                error.append(Scorecard_List[i])
 
-from csv import DictWriter
-
-players = [{'dailyWinners': 3, 'dailyFreePlayed': 2, 'user': 'Player1', 'bank': 0.06},
-{'dailyWinners': 3, 'dailyFreePlayed': 2, 'user': 'Player2', 'bank': 4.0},
-{'dailyWinners': 1, 'dailyFree': 2, 'user': 'Player3', 'bank': 3.1},            
-{'dailyWinners': 3, 'dailyFree': 2, 'user': 'Player4', 'bank': 0.32}]
-
-with open('spreadsheet.csv','w') as outfile:
-    writer = DictWriter(outfile, ('dailyWinners','dailyFreePlayed','dailyFree','user','bank'))
-    writer.writeheader()
-    writer.writerows(players)
+for i in xrange(len(error)):
+    print('Add row to Scorecard:')
+    print error[i]
+           
